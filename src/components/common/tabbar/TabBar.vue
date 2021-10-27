03170166a0e4152a6031e2d81f0578f1408bb8e6
DuanPengZhan <1062838351@qq.com>
2021-10-27 11:22:40 +0800

    tabbar

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style  scoped>
+#tab-bar {
+  display: flex;
+  background-color: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  /* box-shadow: 0 -0.1785rem 0.1785rem 0 #f4f4f4; */
+  border-top: 1px solid #d9d9d9;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+
+const routes = [{
+    path: '',
+    redirect: '/home',
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: () => import('../views/home/Home.vue'),
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: () => import('../views/cart/Cart.vue'),
+  },
+  {
+    path: '/category',
+    name: 'Category',
+    component: () => import('../views/category/Category.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../views/profile/Profile.vue'),
   }
 ]
 
