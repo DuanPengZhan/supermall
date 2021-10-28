261ea114f5f3d907a505eeacc086027649f79906
DuanPengZhan <1062838351@qq.com>
2021-10-28 18:37:43 +0800

    GoodsList

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="goods">
+    <goods-list-item
+      v-for="(item, index) in goods"
+      :key="index"
+      :goods-item="item"
+    >
+    </goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem.vue";
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem,
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* justify-content:space-evenly */
+.goods {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+</style>
--- src/network/home.js
+++ src/network/home.js
@@ -3,7 +3,17 @@ import {
 } from "./request";
 
 export function getHomeMultidata() {
-    return request({ 
+    return request({
         url: '/home/multidata'
+    })
+}
+
+export function getHomeGoods(type, page) {
+    return request({
+        url: '/home/data',
+        params: {
+            type,
+            page
+        }
     })
 }
